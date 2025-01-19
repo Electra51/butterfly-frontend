d7902334bd81793d7de8970dd80eb942f61a2d65
safayet nur <safayet@merchantbay.com>
2025-01-19 12:15:35 +0600

    about section added

---
+++ src/Pages/HomePage/AboutHome.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import aboutImage from "../../assets/bannerLogo/about.png";
+import { BsArrowRightShort } from "react-icons/bs";
+import { Link } from "react-router-dom";
+const AboutHome = () => {
+  return (
+    <div className="care py-20 bg-fixed mt-[180px]">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center max-w-[19rem] lg:max-w-6xl mx-auto ">
+        <div>
+          <img
+            src={aboutImage}
+            alt=""
+            width={500}
+            className="lg:flex hidden transition duration-300 ease-in-out hover:scale-110"
+          />
+        </div>
+        <div className="overlay overlay_2 p-10">
+          <p className="tracking-[0.1rem] text-2xl border-b w-40 lg:w-80 text-start font-bold text-white">
+            About us
+          </p>
+          <p className="text-justify tracking-[0.01rem] mt-5 text-white">
+            The top three occupations in the Beauty salons Industry Group are
+            Hairdressers, hairstylists, & cosmetologists, Manicurists and
+            pedicurists, Receptionists & information clerks, Supervisors of
+            personal care and service workers, and Skincare specialists.We
+            started as a small beauty studio in Islington, London. Our main idea
+            was to create the best beauty studio in the world. Can there be
+            compromises in the best studio in the world? Our answer is always
+            no, we care about the best quality, we hire the best specialists and
+            provide the best customer service.
+          </p>
+          <Link to="/about">
+            <button className="button7 b7 mt-10 text-white">
+              Explore More <BsArrowRightShort className="arrow1 text-2xl" />
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutHome;
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import ExtraFeature from "./ExtraFeature";
+import AboutHome from "./AboutHome";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <ExtraFeature />
+      <AboutHome />
     </div>
   );
 };
